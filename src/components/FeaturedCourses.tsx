4ee8b009a351959efbdc61bc8eb718b244d11590
Anuj Singh <anuj@Anujs-MacBook-Air.local>
2025-07-11 15:19:19 +0530

    feature course sec. added

--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -1,11 +1,14 @@
+'use client'
 import courseData from "../data/music_courses.json"
 import Link from "next/link";
 import { Button } from "./ui/moving-border";
+import { BackgroundGradient } from "./ui/background-gradient";
 
 interface Course{
     id:number,
     title: string,
-    description: number,
+    slug:string,
+    description: string,
     duration: string,
     level: string,
     format: string,
@@ -17,7 +20,7 @@ interface Course{
 // username = 1; // it shows error due to number assign in string type defined
 
 function FeaturedCourses () {
-    const featuredCourses = courseData.courses?.filter((course:Course) => course.isFeatured === true )|| []
+    const featuredCourses = courseData.courses.filter((course:Course) => course.isFeatured)
     return(
         <div className="py-12 bg-gray-900">
             <div>
@@ -35,7 +38,15 @@ function FeaturedCourses () {
                 <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-center">
                     {featuredCourses.map((course:Course) => (
                         <div key={course.id} className="flex justify-center">
-                            <p>test</p>
+                            <BackgroundGradient className="flex flex-col rounded-[22px] bg-white dark:bg-zinc-900 overflow-hidden h-full max-w-sm">
+                                <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
+                                    <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
+                                    <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
+                                    <Link className='mt-5' href={`/courses/${course.slug}`}>
+                                    Learn more
+                                    </Link>
+                                </div>
+                            </BackgroundGradient>
                         </div>
                     ))}
                 </div>
